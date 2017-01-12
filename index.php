e8ce6471d5e2fb35caec162bf274d408133c011d
Taylor McCarthy <oresshi@gmail.com>
2017-01-12 00:42:07 -0700

    Added head and body sections. Added title.

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: blue
- * Date: 1/12/17
- * Time: 12:21 AM
- * upload test
- */
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Steam Community Use Case & Flow Analysis</title>
+	</head>
+	<body>
+		<header>
+			<h1>Hypothetical Persona Use Case & Guide Creation Interaction Flow</h1>
+		</header>
+	</body>
+</html>
