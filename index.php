1f0f128d35fab7475f52aa18ac17540e7ed34096
Taylor McCarthy <oresshi@gmail.com>
2017-01-12 01:43:32 -0700

    Completed "Persona" section. Completed "Use Case" section. Started "Interaction Flow" section.

--- index.php
+++ index.php
@@ -23,8 +23,24 @@
 					Age:28
 				<br><br>
 					Occupation: Airline Pilot
-
+				<br><br>
+					Technology:
+					<ul>
+					<li>Gaming Desktop</li>
+					<li>Lightweight Laptop</li>
+					<li>Smartphone (Palm Pre)</li>
+					</ul>
+					Needs: Frequently travels making gaming difficult due to technology performance constraints. Enjoys interacting with, and creating content for, various games' communities.
+				<br><br>
+					Goals: Create useful content for gaming communities.
 			</p>
+				<br><br>
+			<h2>Use Case</h2>
+			<p>
+					Sara wants to help the other players of the video games she plays by creating informative and easy to understand guides. She is technologically literate, but does not know how to code or create a website. She needs an easy to use GUI solution to creating/hosting/sharing guides for her favorite games.
+			</p>
+			<br><br>
+			<h3>Interaction Flow</h3>
 		</center>
 	</body>
 </html>
