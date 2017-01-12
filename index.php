2fe6e35f9b779ccab47330e3d903a53b9686cc4e
Taylor McCarthy <oresshi@gmail.com>
2017-01-12 00:58:53 -0700

    Centered text, added tags for first text section.

--- index.php
+++ index.php
@@ -6,7 +6,21 @@
 	</head>
 	<body>
 		<header>
-			<h1>Hypothetical Persona Use Case & Guide Creation Interaction Flow</h1>
+			<center> <!-- center is deprecated, but I want it to look pretty and don't want to bother with css now. -->
+				<h1>Hypothetical Persona Use Case & Guide Creation Interaction Flow</h1>
+			</center>
 		</header>
+		<br>
+		<center>
+			<p>
+				Persona
+			</p>
+		</center>
+		<br>
+		<center>
+			<p>
+
+			</p>
+		</center>
 	</body>
 </html>
