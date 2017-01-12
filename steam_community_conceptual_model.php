24f0898df6a0c4a2f25644e7bba461d814d1d464
Taylor McCarthy <oresshi@gmail.com>
2017-01-12 13:11:34 -0700

    Added concept model.

---
+++ steam_community_conceptual_model.php
@@ -0,0 +1,12 @@
+<!DOCTYPE htmL>
+<html lang="en"
+	<head>
+		<meta charset="utf-8"/>
+		<title>Steam Community Conceptual Model</title>
+	</head>
+	<body>
+		<h1>
+			Entities &amp Attributes
+		</h1>
+	</body>
+</html>
