50d52e8b1d4dd64075f641298fe704087304a64c
Taylor McCarthy <oresshi@gmail.com>
2017-01-12 02:12:23 -0700

    Added "interaction flow" section and began added content to it.

--- index.php
+++ index.php
@@ -35,12 +35,41 @@
 					Goals: Create useful content for gaming communities.
 			</p>
 				<br><br>
-			<h2>Use Case</h2>
+			<h3>Use Case</h3>
 			<p>
 					Sara wants to help the other players of the video games she plays by creating informative and easy to understand guides. She is technologically literate, but does not know how to code or create a website. She needs an easy to use GUI solution to creating/hosting/sharing guides for her favorite games.
 			</p>
 			<br><br>
-			<h3>Interaction Flow</h3>
+			<h4>Interaction Flow</h4>
+			<p>
+				<ol>
+					<li>Opens Steam application (inputs username/password if not already logged in)</li>
+					<li>
+						Steam application presents user's pre-selected home page.
+					</li>
+					<li>Sara clicks on "community" button.</li>
+					<li>Steam application displays community hub home page with customized lists based upon the user's recently viewed hubs and recently played games.</li>
+					<li>Sara searches for the game hub the guide is intended for or navigates to desired game hub via recently played/recently view hubs quick links.</li>
+					<li>Steam application displays game hub page.</li>
+					<li>
+						Sara clicks on the "Guides" button.
+					</li>
+					<li>
+						Steam appplication displays "Guides" page for previously selected game hub.
+					</li>
+					<li>
+						Sara clicks on the "Create Guide" button.
+					</li>
+					<li>Steam application shows "Basic Guide Information" page.
+					</li>
+					<li>
+						Sara inputs a title for her guide in the provided text field, clicks the "browse" button to open a file browser and select a branding image for her guide, writes a brief description of her guide in the provided text field, clicks on "category" check boxes corresponding to the categories she wishes her guide to be listed under, selects the "language" check box for the language she will write in, checks box certifying she is the original creator of the guide, and clicks "save and continue" button.
+					</li>
+					<li>
+						Steam uploads and store all data input by Sara.
+					</li>
+				</ol>
+			</p>
 		</center>
 	</body>
 </html>
