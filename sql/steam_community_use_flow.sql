80f68da92e80dd4cfbcb269c524f5b4a3b20bbbd
Taylor McCarthy <oresshi@gmail.com>
2017-01-17 23:47:27 -0700

    Added SQL

--- sql/steam_community_use_flow.sql
+++ sql/steam_community_use_flow.sql
@@ -11,3 +11,26 @@ CREATE TABLE profile (
 	UNIQUE(profileAccountPhone)
 	PRIMARY KEY(profileId)
 );
+CREATE TABLE guide (
+	guideId INT UNSIGNED AUTO INCREMENT NOT NULL,
+	guideOwnerId INT UNSIGNED NOT NULL,
+	guideLanguage VARCHAR (32) NOT NULL,
+	guideCatagory VARCHAR (32) NOT NULL,
+	guideContent VARCHAR (2048) NOT NULL,
+	guideCreateDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	guideModifyDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	INDEX(guideOwnerId),
+	FOREIGN KEY(guideOwnerId) REFERENCES profile(profileId)
+	PRIMARY KEY(guideId)
+);
+
+CREATE TABLE guideProfile(
+	guideProfileProfileId INT UNSIGNED NOT NULL,
+	guideProfileGuideId INT UNSIGNED NOT NULL,
+	INDEX(guideProfileProfileId),
+	INDEX(guideProfileGuideId),
+	FOREIGN KEY(guideProfileProfileId) REFERENCES profile(profileId),
+	FOREIGN KEY(guideProfileGuideId) REFERENCES guide(guideId),
+	PRIMARY KEY(guideProfileProfileId, guideProfileGuideId)
+);
+
