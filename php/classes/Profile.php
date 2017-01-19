46d51184f5e54e02077b335eb7835e7d549bb002
Taylor McCarthy <oresshi@gmail.com>
2017-01-19 11:35:21 -0700

    Updated Doc Block.

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -1,11 +1,12 @@
 <?php
 namespace edu\cnm\Tmccarthy4\steam_community_use_flow;
 /**
- * Created by PhpStorm.
- * User: blue
- * Date: 1/19/17
- * Time: 8:29 AM
- * Create Profile Class, defines user id (primary key), user email and user phone.
+ *Profile Id and contact/account verification info.
+ *
+ * This stores and displays the user's profile Id, email address, and phone number.
+ *
+ * @author Taylor McCarthy <tmmccarthy4@cnm.edu>
+ * @version 1.0
  **/
 class Profile {
 	/**
