b622c7cc1eee2c05f2c0b51880c4dee5e0311333
Taylor McCarthy <oresshi@gmail.com>
2017-01-19 11:22:33 -0700

    Created return and mutators for profileId, profileAccountEmail, profileAccountPhone.

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -31,21 +31,31 @@ class Profile {
 	public function getProfileId() {
 		return ($this->profileId);
 	}
-
+	/**
+	 *mutator for profileId
+	 * @param int $setProfileId new value of profileId
+	 * @throws \InvalidArgumentException if $setProfileId is insecure
+	 * @throws \TypeError if $setProfileId is not an integer
+	 **/
+	public function setProfileId(int $setProfileId = null) {
+		// if profileId is null, this is a new profile without a mmySQL assigned id
+		if($setProfileId === null) {
+			$this->profileId = null;
+			return;
+		}
+		//verify profileId is positive
+		if($setProfileId <= 0) {
+			throw(new \RangeException("Profile Id is not positive"));
+		}
+		//convert and store the profile id
+		$this->profileId = $setProfileId;
+	}
 	/**
 	 * accessor method for profileAccountEmail.
 	 * @return string value of profileAccountEmail.
 	 **/
 	public function getProfileAccountEmail() {
 		return ($this->profileAccountEmail);
-	}
-
-	/**
-	 * accessor method for profileAccountPhone
-	 * @return int value of profileAccountPhone
-	 **/
-	public function getProfileAccountPhone() {
-		return ($this->profileAccountPhone);
 	}
 
 	/**
@@ -69,4 +79,34 @@ class Profile {
 		//store the email
 		$this->profileAccountEmail = $setProfileAccountEmail;
 	}
+
+	/**
+	 * accessor method for profileAccountPhone
+	 * @return int value of profileAccountPhone
+	 **/
+	public function getProfileAccountPhone() {
+		return ($this->profileAccountPhone);
+	}
+	/**
+	 * mutator for profileAccountPhone
+	 * @param string $setProfileAccountPhone new value of profileAccountPhone
+	 * @throws \InvalidArgumentException if $setProfileAccountPhone is not a string or is insecure
+	 * @throws \RangeException if $setProfileAccountPhone is > 32 characters
+	 * @throws \TypeError if $setProfileAccountPhone is not a string
+	 **/
+	public function setProfileAccountPhone (string $setProfileAccountPhone) {
+		//verify the content is secure
+		$setProfileAccountPhone = trim($setProfileAccountPhone);
+		$setProfileAccountPhone = filter_var($setProfileAccountPhone, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($setProfileAccountPhone) === true) {
+			throw(new \InvalidArgumentException("Phone number cannot be blank or insecure."));
+		}
+		//verify the phone number will fit in the database
+		if(strlen($setProfileAccountPhone) > 32) {
+			throw(new \RangeException("Phone number cannot be longer than 32 characters."));
+		}
+		//store the new phone number
+		$this->profileAccountPhone = $setProfileAccountPhone;
+	}
+
 }
