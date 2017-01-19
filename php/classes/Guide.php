801d51e24ebf771a1704fbde1d2f4f5e59d5653b
Taylor McCarthy <oresshi@gmail.com>
2017-01-19 12:01:37 -0700

    Updated Doc Block for Guide class.

--- php/classes/Guide.php
+++ php/classes/Guide.php
@@ -3,6 +3,9 @@
  *
  * Steam Guide Main Content
  *
- * This modifies
+ * This modifies and displays a user's guide's content and info.
  *
+ * @author Taylor McCarthy <tmccarthy4@cnm.edu>
+ * @version 1.0
  */
+
